ca94a46e32ada2eb748483bda8bdc32269a17b5b
akpleo <191282414+akpleo@users.noreply.github.com>
2025-10-06 18:36:51 +0530

    Add files via upload

---
+++ chckUpdIsin.sql
@@ -0,0 +1,46 @@
+declare
+--Create chckUpdIsin(
+@chckCount char = 'N' ,
+@editInsin int = 0 ,
+@issplit int = 0,
+@isbonus int = 0,
+@splitRat int = 0,
+@oldIsin varchar(12)='',
+@newIsin varchar(12)=''
+--)
+begin
+
+if @chckCount = 'Y'
+
+begin
+exec checkCount @oldIsin, @newIsin
+
+end
+
+if @editInsin  = 0
+
+begin 
+
+if @issplit = 1
+
+begin 
+
+update CTWIO 
+set tio_tISIN=@newIsin,
+tio_lQty=FLOOR(tio_lQty * @splitRat)  --- Original Qty * Split Ratio
+where tio_tISIN=@oldIsin
+
+end
+if @isbonus  = 1
+
+declare 
+@scrsrno int
+select @scrsrno=scp_lSrNo from CMSCRP(nolock) where scp_tIsin=@newIsin
+
+update CTWIO 
+set tio_tISIN=@newIsin,
+tio_lScpSrNo=@scrsrno
+where tio_tISIN=@oldIsin
+
+end
+end
---
+++ checkCount.sql
@@ -0,0 +1,20 @@
+Declare
+--Create checkCount(
+@oldIsin varchar(12)='INE485C01011',
+@newIsin varchar(12)='INE485C01029'
+--)
+--drop table #CTWIO_tmp
+
+begin 
+	
+	select * into #CTWIO_tmp from CTWIO with (nolock)  where tio_tISIN in (@oldIsin,@newIsin)
+
+---=====================checking count 
+
+	select count (*) as 'count',tio_tISIN as ISIN from #CTWIO_tmp with (nolock) group by tio_tISIN
+
+---=====================checking quantity 
+
+	select sum(tio_lqty) as qty,tio_tISIN as ISIN from #CTWIO_tmp group by tio_tISIN
+
+end
